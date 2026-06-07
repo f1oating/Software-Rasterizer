37be1f12bb2fc0abf6b9575ff59bf7e99c7402b2
f1oating <dtoronchenko@gmail.com>
2026-06-07 10:58:17 +0100

    vec operations

--- src/Geometry.h
+++ src/Geometry.h
@@ -21,3 +21,94 @@ double operator*(const vec<n>& lhs, const vec<n>& rhs)
     }
     return ret;
 }
+
+template<int n>
+vec<n> operator+(const vec<n>& lhs, const vec<n>& rhs)
+{
+    vec<n> ret;
+    for (int i = 0; i < n; i++)
+    {
+        ret[i] = lhs[i] + rhs[i];
+    }
+    return ret;
+}
+
+template<int n>
+vec<n> operator-(const vec<n>& lhs, const vec<n>& rhs)
+{
+    vec<n> ret;
+    for (int i = 0; i < n; i++)
+    {
+        ret[i] = lhs[i] - rhs[i];
+    }
+    return ret;
+}
+
+template<int n>
+vec<n> operator*(const vec<n>& lhs, const double& rhs)
+{
+    vec<n> ret;
+    for (int i = 0; i < n; i++)
+    {
+        ret[i] = lhs[i] * rhs;
+    }
+    return ret;
+}
+
+template<int n>
+vec<n> operator*(const double& lhs, const vec<n>& rhs)
+{
+    return rhs * lhs;
+}
+
+template<int n>
+vec<n> operator/(const vec<n>& lhs, const vec<n>& rhs)
+{
+    vec<n> ret;
+    for (int i = 0; i < n; i++)
+    {
+        ret = lhs[i] / rhs[i];
+    }
+    return ret;
+}
+
+template<>
+struct vec<2> 
+{
+    double x = 0;
+    double y = 0;
+
+    double& operator[](const int i) { assert(i>=0 && i<2); return i ? y : x; }
+    double operator[](const int i) const { assert(i>=0 && i<2); return i ? y : x; }
+};
+
+template<> 
+struct vec<3> 
+{
+    double x = 0;
+    double y = 0;
+    double z = 0;
+
+    double& operator[](const int i) { assert(i>=0 && i<3); return i ? (1==i ? y : z) : x; }
+    double operator[](const int i) const { assert(i>=0 && i<3); return i ? (1==i ? y : z) : x; }
+};
+
+template<> 
+struct vec<4> 
+{
+    double x = 0;
+    double y = 0;
+    double z = 0;
+    double w = 0;
+
+    double& operator[](const int i) { assert(i>=0 && i<4); return i<2 ? (i ? y : x) : (2==i ? z : w); }
+    double operator[](const int i) const { assert(i>=0 && i<4); return i<2 ? (i ? y : x) : (2==i ? z : w); }
+
+    vec<2> xy() const { return {x, y}; }
+    vec<3> xyz() const { return {x, y, z}; }
+};
+
+typedef vec<2> vec2;
+typedef vec<3> vec3;
+typedef vec<4> vec4;
+
